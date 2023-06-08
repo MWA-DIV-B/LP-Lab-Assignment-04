4c55c297c4ff7aa9aafabcfa06d194f143d84d7d
Rohit <90854263+zephyr0p@users.noreply.github.com>
2023-06-08 19:56:21 +0530

    Add javascript file

---
+++ CS3226/script.js
@@ -0,0 +1,143 @@
+var sound1 = new Audio();
+sound1.src = "/Task-Audios/src/ambulance sound.wav"
+
+var PlayPauseBTN1 = document.getElementById('PlayPauseBTN1');
+var count = 0;
+function playpause1() {
+    if (count == 0) {
+        count = 1;
+        sound1.play();
+        sound1.loop = true;
+    } else {
+        count = 0;
+        sound1.pause();
+    }
+}
+
+var sound2 = new Audio();
+sound2.src = "/Task-Audios/src/birds.wav"
+
+var PlayPauseBTN2 = document.getElementById('PlayPauseBTN2');
+var count = 0;
+function playpause2() {
+    if (count == 0) {
+        count = 1;
+        sound2.play();
+        sound2.loop = true;
+    } else {
+        count = 0;
+        sound2.pause();
+    }
+}
+
+var sound3 = new Audio();
+sound3.src = "/Task-Audios/src/camp_fire.wav"
+
+var PlayPauseBTN3 = document.getElementById('PlayPauseBTN3');
+var count = 0;
+function playpause3() {
+    if (count == 0) {
+        count = 1;
+        sound3.play();
+        sound3.loop = true;
+    } else {
+        count = 0;
+        sound3.pause();
+    }
+}
+
+var sound4 = new Audio();
+sound4.src = "/Task-Audios/src/chilren_audience.wav"
+
+var PlayPauseBTN4 = document.getElementById('PlayPauseBTN4');
+var count = 0;
+function playpause4() {
+    if (count == 0) {
+        count = 1;
+        sound4.play();
+        sound4.loop = true;
+    } else {
+        count = 0;
+        sound4.pause();
+    }
+}
+
+var sound5 = new Audio();
+sound5.src = "/Task-Audios/src/city_road.wav"
+
+var PlayPauseBTN5 = document.getElementById('PlayPauseBTN5');
+var count = 0;
+function playpause5() {
+    if (count == 0) {
+        count = 1;
+        sound5.play();
+        sound5.loop = true;
+    } else {
+        count = 0;
+        sound5.pause();
+    }
+}
+
+var sound6 = new Audio();
+sound6.src = "/Task-Audios/src/clock_timer.wav"
+
+var PlayPauseBTN6 = document.getElementById('PlayPauseBTN6');
+var count = 0;
+function playpause6() {
+    if (count == 0) {
+        count = 1;
+        sound6.play();
+        sound6.loop = true;
+    } else {
+        count = 0;
+        sound6.pause();
+    }
+}
+
+var sound7 = new Audio();
+sound7.src = "/Task-Audios/src/rain.mp3"
+
+var PlayPauseBTN7 = document.getElementById('PlayPauseBTN7');
+var count = 0;
+function playpause7() {
+    if (count == 0) {
+        count = 1;
+        sound7.play();
+        sound7.loop = true;
+    } else {
+        count = 0;
+        sound7.pause();
+    }
+}
+
+var sound8 = new Audio();
+sound8.src = "/Task-Audios/src/water_waves.wav"
+
+var PlayPauseBTN8 = document.getElementById('PlayPauseBTN8');
+var count = 0;
+function playpause8() {
+    if (count == 0) {
+        count = 1;
+        sound8.play();
+        sound8.loop = true;
+    } else {
+        count = 0;
+        sound8.pause();
+    }
+}
+
+var sound9 = new Audio();
+sound9.src = "/Task-Audios/src/wind.wav"
+
+var PlayPauseBTN9 = document.getElementById('PlayPauseBTN9');
+var count = 0;
+function playpause9() {
+    if (count == 0) {
+        count = 1;
+        sound9.play();
+        sound9.loop = true;
+    } else {
+        count = 0;
+        sound9.pause();
+    }
+}
