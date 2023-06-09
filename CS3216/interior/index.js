f0a98147e7505bcd7900958a8eb6e6616e917d7c
maithilee66 <110760285+maithilee66@users.noreply.github.com>
2023-06-09 13:23:15 +0530

    Add files via upload

---
+++ CS3216/interior/index.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+//     $('.fa-bars').click(function(){
+//        $(this).toggleClass('fa-times');
+//        $('nav').toggleClass('nav-toggle');
+//     });
+
+    
+// });
+
+// $('.project').magnificPopup({
+//     delegate:'a',
+//     type:'image',
+//     gallery:{
+//         enabled:true
+//     }
+// })
